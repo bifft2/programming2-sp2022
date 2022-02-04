deec07b32c246dca475f51688f69ee17c584dfe2
bifft2 <bifft95@gmail.com>
2022-02-04 15:38:20 -0600

    add list notes

---
+++ Notes/lists.py
@@ -0,0 +1,19 @@
+class_list = ["Alberto", "Jonah", "Ryder", "Malcolm"]
+print(class_list)
+
+# get elements at specific indices
+print(class_list[0])
+print(class_list[3])
+print(class_list[-1])
+
+# get the length of the list
+print(len(class_list))
+
+# append to a list
+class_list.append("Ms. Ifft")
+print(class_list)
+
+# loop through a list
+for i in range(len(class_list)):
+    print(class_list[i])
+
