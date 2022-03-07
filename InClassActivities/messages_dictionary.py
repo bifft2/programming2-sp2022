cba7203f61088e0913fec680ed7fbe8ba9ee0153
bifft2 <bifft95@gmail.com>
2022-03-07 10:31:46 -0600

    add messages_dictionary.py

---
+++ InClassActivities/messages_dictionary.py
@@ -0,0 +1,13 @@
+text_messages = {}
+
+def send_message(text_messages, name, message):
+    pass
+
+
+def get_messages(text_messages, name):
+    pass
+
+
+send_message(text_messages, "Jonah", "hello")
+send_message(text_messages, "Beckett", "What time is dinner?")
+send_message(text_messages, "Beckett", "Feed the dog.")
