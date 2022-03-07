912e86c32ca9ffec338d442196229f8f3643da82
bifft2 <bifft95@gmail.com>
2022-03-07 10:49:09 -0600

    update messages_dictionary.py

--- InClassActivities/messages_dictionary.py
+++ InClassActivities/messages_dictionary.py
@@ -1,13 +1,21 @@
 text_messages = {}
 
 def send_message(text_messages, name, message):
-    pass
-
+    if name in text_messages:
+        text_messages[name].append(message)
+    else:
+        text_messages[name] = [message]
 
 def get_messages(text_messages, name):
-    pass
+    return text_messages.get(name)
 
 
 send_message(text_messages, "Jonah", "hello")
 send_message(text_messages, "Beckett", "What time is dinner?")
 send_message(text_messages, "Beckett", "Feed the dog.")
+beckett_messages = get_messages(text_messages, "Beckett")
+alberto_messages = get_messages(text_messages, "Alberto")
+
+print(text_messages)
+print(beckett_messages)
+print(alberto_messages)
